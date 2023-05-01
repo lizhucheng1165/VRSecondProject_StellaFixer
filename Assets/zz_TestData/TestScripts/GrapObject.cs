e3c2e0a9717e21d833fb78eff0127a944bae50d2
lizhucheng1165 <lizhucheng18@gmail.com>
2023-05-01 20:18:41 +0900

    Door Test 01
    Door Test 01

--- Assets/zz_TestData/TestScripts/GrapObject.cs
+++ Assets/zz_TestData/TestScripts/GrapObject.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class GrapObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+        {
+            other.transform.SetParent(this.transform);
+            other.transform.GetComponent<Rigidbody>().isKinematic = true;
+            //float x = other.transform.parent.rotation.x;
+            //float y = other.transform.parent.rotation.y + Vector3.Angle(other.transform.position, this.transform.position)/ 10;
+            //float z = other.transform.parent.rotation.z;
+
+            //other.transform.parent.rotation = Quaternion.Euler(x, Mathf.Clamp(y,0,-120f ), z);
+        }
+        else
+        {
+            other.transform.SetParent(null);
+            other.transform.GetComponent<Rigidbody>().isKinematic = false;
+
+        }
     }
 }
